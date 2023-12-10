b9677262ad17ad4401f9cbc4ec40c62d166674b9
Ahmed Zamouche <ahmed.zamouche@tobii.com>
2023-12-10 19:44:07 +0100

    change docker term with float shell

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -15,14 +15,14 @@ return {
       -- float bash
       { "<leader>tf", "<cmd>lua TermToggleShell('float')<cr>", desc = "Shell" },
       {
-        "<a-2>",
+        "<a-3>",
         mode = { "n", "v", "t" },
         "<cmd> lua TermToggleShell('float')<cr>",
         desc = "Shell",
       },
       -- containers
       { "<leader>tc", "<cmd>lua TermToggleContainer('float')<cr>", desc = "Container" },
-      { "<a-3>", mode = { "n", "x", "o", "t" }, "<cmd>lua TermToggleContainer('float')<cr>", desc = "Container" },
+      { "<a-2>", mode = { "n", "x", "o", "t" }, "<cmd>lua TermToggleContainer('float')<cr>", desc = "Container" },
       -- ssh
       { "<leader>ts", "<cmd>lua TermToggleSsh('float')<cr>", desc = "ssh" },
       { "<a-4>", mode = { "n", "x", "o", "t" }, "<cmd>lua TermToggleSsh('float')<cr>", desc = "ssh" },
