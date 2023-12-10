105d9d7146724c14e74b36d5c476611d1862a3a2
Ahmed Zamouche <ahmed.zamouche@tobii.com>
2023-12-10 19:43:22 +0100

    encapsulate docker functions

--- lua/user/docker.lua
+++ lua/user/docker.lua
@@ -1,41 +1,41 @@
-json = require("thirdparty.json.json")
-utils = require("user.utils")
+local json = require("thirdparty.json.json")
+local utils = require("user.utils")
 
 local M = {}
 
 function M.container(args)
   args = args or { cmd = "list" }
 
-  function list()
+  local function list()
     local proc = io.popen("docker ps -as  --no-trunc --format json", "r")
     if proc == nil then
       return false
     end
 
-    lst = {}
+    local lst = {}
     for line in proc:lines() do
-      t = json.decode(line)
+      local t = json.decode(line)
       lst[t.Image] = t
     end
     return true, lst
   end
 
-  function start(cid)
+  local function start(cid)
     local suc, _, _ = os.execute("docker start " .. cid .. " > /dev/null 2>&1")
     return suc ~= nil and suc
   end
 
-  function stop(cid)
+  local function stop(cid)
     local suc, _, _ = os.execute("docker stop " .. cid .. " > /dev/null 2>&1")
     return suc ~= nil and suc
   end
 
-  function restart(cid)
+  local function restart(cid)
     stop(cid)
     return start(cid)
   end
 
-  function attach(cid, callback)
+  local function attach(cid, callback)
     return start(cid) and callback(cid)
   end
 
