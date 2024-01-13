4e8b51dd285d599b8a5da0dd3def0db446d9485a
Ahmed Zamouche <ahmed.zamouche@prevas.se>
2024-01-13 10:28:20 +0100

    set powershell default shell in win

--- init.lua
+++ init.lua
@@ -3,3 +3,10 @@ require("config.lazy")
 require("user.toggleterm")
 require("user.csearchpath")
 require("user.weather")
+
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  vim.opt.shell = "powershell"
+  vim.opt.shellcmdflag = "-command"
+  vim.opt.shellquote = '"'
+  vim.opt.shellxquote = ""
+end
