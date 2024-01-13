0be14582c9d2922b82fd25cc100a771f19bb3090
Ahmed Zamouche <ahmed.zamouche@prevas.se>
2024-01-13 10:29:46 +0100

    update utils get os name

--- init.lua
+++ init.lua
@@ -5,6 +5,8 @@ require("user.csearchpath")
 require("user.weather")
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
+  --require("nvim-treesitter.install").compilers = { "gcc" }
+  require("nvim-treesitter.install").prefer_git = false
   vim.opt.shell = "powershell"
   vim.opt.shellcmdflag = "-command"
   vim.opt.shellquote = '"'
